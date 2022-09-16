a76816da10eb04bc150512ace27dd8d4df3b6c84
olaide-hok <olaidekareem43@yahoo.com>
2022-09-16 10:17:47 +0100

    Update README.md

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
+
 function App() {
   return <h2>pagination starter</h2>
 }
